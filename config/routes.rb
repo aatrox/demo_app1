d12ba01785b4df63c7e215b470286bdead1e0c1a
Di Wu <diwuusa@gmail.com>
2013-09-15 16:50:13 -0500

    create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 DemoApp1::Application.routes.draw do
   resources :users
 
+  root to: 'static_pages#home'
+  # match '/', to: 'static_pages', via: 'get'
+  # match '/', to: 'application', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
