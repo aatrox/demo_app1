ba5e222b62afc2c8a741b82932df42c2269023d6
Di Wu <diwuusa@gmail.com>
2013-09-17 18:14:54 -0500

    fix root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ DemoApp1::Application.routes.draw do
   resources :microposts
   resources :users
 
-  root to: 'static_pages#home'
-  match '/microposts/new', to: 'microposts#new', via: 'get'
+  root to: 'microposts#index'
 end
